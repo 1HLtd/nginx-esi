8c8e5f06f0989be173fe97ccbf93b0fc6b83fc2e
Marian Marinov <mm@yuhu.biz>
2015-02-25 05:02:25 +0200

    ngx_esi_tag.c: removed esi_tag_alloc_buf as it is not used anywhare and generates build errors
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- ngx_esi_tag.c
+++ ngx_esi_tag.c
@@ -240,24 +240,6 @@ ESITag *esi_tag_close_children( ESITag *tag, esi_tag_t type )
   return NULL;
 }
 
-static ngx_buf_t *esi_tag_alloc_buf(ESITag *tag, const void *data, size_t length)
-{
-  ngx_buf_t *b;
-  b = ngx_pcalloc(tag->ctx->request->pool, sizeof(ngx_buf_t));
-  if (b == NULL) {
-    ngx_log_error(NGX_LOG_ERR, tag->ctx->request->connection->log, 0, "Failed to allocate response buffer.");
-   // return NGX_HTTP_INTERNAL_SERVER_ERROR;
-   return NULL;
-  }
-  b->pos = (u_char*)(data); /* first position in memory of the data */
-  b->last = (((u_char*)data) + length); /* last position */
-
-  b->memory = 1; /* content is in read-only memory */
-               /* (i.e., filters should copy it rather than rewrite in place) */
-  b->last_buf = 0;
-  return b;
-}
-
 ngx_buf_t *esi_tag_buffer(ESITag *tag, const void *data, size_t length)
 {
   //printf("buffer: %lu for tag: ", length); esi_tag_debug(tag);
